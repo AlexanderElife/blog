85833431a08992c63cb001609ee6db47538fef16
Alexander Conterón <alexander@elifetransfer.com>
2024-04-02 12:29:20 -0500

    Plataforma cambios

---
+++ js/master.js
@@ -0,0 +1,9 @@
+document.getElementById("login").onclick = function() {
+    window.location.href='admin/dashboard.php';
+    
+ };
+
+ document.getElementById("invite").onclick = function() {
+    window.location.href='home.php';
+    
+ }; 
